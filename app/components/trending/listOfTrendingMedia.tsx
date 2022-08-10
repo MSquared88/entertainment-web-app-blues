5c036819ec55d8bbf270d65cc45725082ee59dfa
Matthew Meeves <39702456+MSquared88@users.noreply.github.com>
2022-08-09 21:32:33 -0500

    added draggable scrolling to trending feature

--- app/components/trending/listOfTrendingMedia.tsx
+++ app/components/trending/listOfTrendingMedia.tsx
@@ -1,4 +1,6 @@
 import type { Media } from "@prisma/client";
+import React from "react";
+import { useDraggable } from "react-use-draggable-scroll";
 
 import { TrendingListItem } from "~/components/trending/trendingListItem";
 
@@ -10,11 +12,19 @@ export default function TrendingMediaDisplay({
   userBookmarksIds?: string[];
   children?: React.ReactChild | React.ReactChild[];
 }) {
+  const ref =
+    React.useRef<HTMLUListElement>() as React.MutableRefObject<HTMLUListElement>;
+  const { events } = useDraggable(ref);
+
   return (
     <div className="flex flex-col">
       <h1 className="text-3xl text-white">Trending</h1>
       {
-        <ul className="grid auto-cols-[500px] grid-flow-col overflow-x-auto py-4 scrollbar scrollbar-track-blue-semi scrollbar-thumb-blue-grayish ">
+        <ul
+          className="grid auto-cols-[500px] grid-flow-col overflow-x-auto py-4 scrollbar scrollbar-track-blue-semi scrollbar-thumb-blue-grayish "
+          {...events}
+          ref={ref}
+        >
           {mediaListItems.map((media) => (
             <TrendingListItem
               userBookmarksIds={userBookmarksIds}
